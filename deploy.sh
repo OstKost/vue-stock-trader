efbcfe37da78e62d1f88565bb308d1dc68b6e425
Konstantin Ostrovskiy <web@ostkost.ru>
2020-04-06 09:34:06 +0300

    add deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,7 @@
 set -e
 
 # сборка
+echo 'Run build...'
 npm run build
 
 # переход в каталог сборки
@@ -12,6 +13,7 @@ cd dist
 # если вы публикуете на пользовательский домен
 # echo 'www.example.com' > CNAME
 
+echo 'Git init...'
 git init
 git add -A
 git commit -m 'deploy'
@@ -20,6 +22,7 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # если вы публикуете по адресу https://<USERNAME>.github.io/<REPO>
+echo 'Git push...'
  git push -f git@github.com:ostkost/vue-stock-trader.git master:gh-pages
 
 cd -
